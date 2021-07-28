c18cd49e4fba46bb95f6f1dbcc813d2a7644b8c9
Lucas Hendrek <31413396+KingPack@users.noreply.github.com>
2021-07-28 02:41:23 +0000

    9

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-Test
+# code test 
+
+a = 10
+
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao =  a * b
+divisao = a / b
+resto = a % b
+
+print (type (soma))
+
+soma = str(soma)
+print(soma)
+print (subtracao)
+print(multiplicacao)
+print (divisao)
+print(resto)
