b9b79c564ca9a2354f52f5a26761c3e8421de10a
KingPack <llhendrek@gmail.com>
2021-08-06 00:05:19 -0300

    off

--- Aula2.py
+++ Aula2.py
@@ -1,2 +1,12 @@
-a = int(input('primeiro valor'))
-b = int(input('segundo valor'))
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+
+if a > b and a > c:
+    print('O maior numero é {}'.format(a))
+elif b > a and b > c:
+    print('o maior numero é {}'.format(b))
+else:
+    print('o maior numero é {}'.format(c))  
+
+print('Final do programa')
